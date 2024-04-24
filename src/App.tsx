737b2d1a70835279d9d1fa281eae46d3b12eb85f
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2024-04-24 18:11:56 +0530

    Feat : Navigation added in Entire Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { Route, Routes } from "react-router-dom";
+
 import SigninForm from "./_auth/forms/SigninForm";
-import { Home } from "./_root/pages";
+import {
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
 import Signupform from "./_auth/forms/Signupform";
 import AuthLayout from "./_auth/forms/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import CreatePost from "./_root/pages/CreatePost";
+import EditPost from "./_root/pages/EditPost";
+import AllUsers from "./_root/pages/AllUsers";
 
 const App = () => {
   return (
@@ -19,6 +30,14 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
       <Toaster />
